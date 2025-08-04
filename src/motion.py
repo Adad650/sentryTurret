8dc0cac3830877fb250d07dcab6d0cba23a02fb2
adi <adi.takhar@gmail.com>
2025-08-04 16:18:54 -0400

    Idk

--- src/motion.py
+++ src/motion.py
@@ -6,6 +6,13 @@ cap.set(cv2.CAP_PROP_FPS, 60)
 ret, old = cap.read()
 old = cv2.cvtColor(old, cv2.COLOR_BGR2GRAY)
 
+def mapMotion(x, y):
+    mappedX = (x/1920) * 100
+    mappedY = (y/1080) * 100
+    print (f"x = {round(mappedX)}%")
+    print (f"y = {round(mappedY)}%\n")
+    return 0
+
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -14,14 +21,19 @@ while True:
     contours, _ = cv2.findContours(thresh, 1, 2)
     if contours:
         c = max(contours, key=cv2.contourArea)
-        if cv2.contourArea(c) > 500:
+        if cv2.contourArea(c) > 5000:
             x, y, w, h = cv2.boundingRect(c)
+            center_x = x + w // 2
+            center_y = y + h // 2
+            #print(f"Motion center: ({center_x}, {center_y})")
+            mapMotion(center_x + 1, center_y + 1)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    # Resize the frame to 1080p resolution
-    resized_frame = cv2.resize(frame, (3840, 2160))  # Resize to 3840x2160 (4K)
-    cv2.imshow("footage", resized_frame)
-    
+
+    # Resize to 4K, then mirror horizontally
+    resized_frame = cv2.resize(frame, (1920, 1080))
+    mirrored = cv2.flip(resized_frame, 1)  # 1=horiz, 0=vert, -1=both
+    cv2.imshow("footage", mirrored)
+
     old = gray
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
